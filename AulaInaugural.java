63ef8c41b93ca18be2fa6aa98eb62446b692a7c6
claudio-es-andrade <kakuya@debian-BULLSEYE-live-builder-AMD64>
2022-07-31 01:11:51 -0300

    Inicio dos Exercícios

---
+++ AulaInaugural.java
@@ -0,0 +1,10 @@
+package edu.claudio.primeirasemana;
+
+public class AulaInaugural {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Primeiro Programa Java: Olá Mundo!!");
+	}
+
+}
