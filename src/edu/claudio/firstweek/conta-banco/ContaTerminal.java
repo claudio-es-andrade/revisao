81e148bb233a34535c591265fbb9acabf485d5b6
claudio-es-andrade <claudioe.s.andrade@gmail.com>
2022-08-01 17:47:46 -0300

    Primeiro Passo

--- src/edu/claudio/firstweek/conta-banco/ContaTerminal.java
+++ src/edu/claudio/firstweek/conta-banco/ContaTerminal.java
@@ -2,6 +2,6 @@ package edu.claudio.firstweek.conta-banco;
 
 public class ContaTerminal {
     public static void main(String[] args) {
-        
+        System.out.println("Teste inicial");
     }
 }
